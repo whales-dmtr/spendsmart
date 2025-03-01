f9d07a06aa49b7304a1f681033020dbf017cbdb1
whales-dmtr <dmytrooliinyk2803@gmail.com>
2025-03-01 15:36:26 +0200

    Create pyproject.toml, set dependencies in it, configure poetry.

--- venvspendsmart/bin/activate.fish
+++ venvspendsmart/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/dima/Documents/spendsmart/venvspendsmart"
+set -gx VIRTUAL_ENV "/Users/dima/Documents/programming/spendsmart/venvspendsmart"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
